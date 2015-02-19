530e817bbd8818e85650e8b155d2dc3a0fd15204
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2015-02-19 10:12:39 +0900

    Add link to screen_name

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 module AccountDecorator
 
+  def linked_screen_name
+    link_to "@#{screen_name}", "https://twitter.com/#{screen_name}", target:'_blank'
+  end
+
   def colored_status
     color = {
       on:  "#444",
